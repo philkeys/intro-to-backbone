2dce49b806b8e1e894f36e8b93ef2669c914e1a7
Phil Keys <phil@phillipkeys.com>
2015-07-15 12:28:27 -0700

    Implements model collection and a collection view to display people

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //$(document).ready(function() {
 
 
-  // This is out person model
+  // This is our person model
   var Person = Backbone.Model.extend({
     defaults: {
       name: 'Guest',
@@ -40,15 +40,19 @@
     tagName: 'li',
     className: 'person',
     // will be automatically called when the view is initiated
-    initialize: function() {
-      this.render();
-    },
+    // We removed initialize so that we can call render() directly
+    // and make use of the chaining method
+    // initialize: function() {
+    //   this.render();
+    // },
 
-    my_template: _.template('<strong><%= name %></strong> (<%= age %>) - <%= occupation %>'),
+    template: _.template($('#personTemplate').html()),
 
     render: function() {
       // remember that toJSON just passes the object parameters so this gets the right data
-      this.$el.html( this.my_template( this.model.toJSON )));
+      this.$el.html( this.template( this.model.toJSON()));
+
+      return this; // return this for chaining
     }
     // this will render out the output for the data of the model associated to this view
     //render: function() {
@@ -83,9 +87,61 @@
   // render method will call template via my_template and pass data to my_template
   // my_template will accept the parameters and assign proper values and return back to render
 
+// This is our model collection maker
+// Note that this doesn't create a new collection, one must create an instance
+// var peopleCollection = new PeopleCollection;
+var PeopleCollection = Backbone.Collection.extend({
+  model: Person
+});
+
+// var person = new Person;
+// var personView = new PersonView({ model: person });
+// var peopleCollection = new PeopleCollection();
+// peopleCollection.add(person);
+// var person2 = new Person({name: "Mohit Jain", age: 25, occupation: "Software Developer"});
+// var personView2 = new PersonView({ model: person2 });
+// peopleCollection.add(person2);
+// peopleCollection
+// peopleCollection.toJSON(); // --> returns a list of data attributes
 
 
+// So now we want to create a collection view of the many people we are going to create...
+// Each collection will be a ul container, this works as the models will be li's
 
+// We will need to loop over each person in a model collection, create a singular view
+// and then put them into the collection view
+// 1. We need to loop over each model (person object)
+// 2. We should call render for each person object
+// 3. We should then display the collection as a whole in the HTML
+var PeopleView = Backbone.View.extend({
+  tagName: 'ul',
+
+  initialize: function() {
+    console.log(this.collection);
+  },
+
+  render: function() {
+    //console.log('this inside the render', this);
+    // Loop over all the person objects
+    // REMEMBER!!!!! You must include the context 'this' in the for loop
+    // otherwise 'this' will refer to the window
+    this.collection.each(function(person) {
+      //console.log('this inside the collection loop', this);
+      // console.log('This is each person object', person);
+      // Should call render for the person objects
+      // The initialize in each person view will call the render for it
+      var personView = new PersonView({ model: person });
+      // console.log('These is each person view\'s html', personView.el);
+
+      // We need 'this' to be the right context so that we an actually append
+      // each new model view to the 'ul'. And to access the 'ul' as well as
+      // each model's 'li' we MUST use 'el.'
+      this.$el.append(personView.render().el);
+    }, this);
+
+    return this; // Always return 'this' from the render call so that you can chain!!
+  }
+});
 
 
 
